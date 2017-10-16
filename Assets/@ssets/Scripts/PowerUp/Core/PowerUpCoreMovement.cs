3dc83f3689a69ecabb54fe0415e9fe435923ce1f
Anshori <anshori.touchten@gmail.com>
2017-10-16 17:15:55 +0700

    - Movement Fix for powerUpCore if they're above 0 of y cordinate or below 0

--- Assets/@ssets/Scripts/PowerUp/Core/PowerUpCoreMovement.cs
+++ Assets/@ssets/Scripts/PowerUp/Core/PowerUpCoreMovement.cs
@@ -9,16 +9,27 @@ public class PowerUpCoreMovement : MonoBehaviour {
 
     float speed = 15f;
     float turningSpeed = 7.5f;
+    public bool isClockWise;
 
     // Use this for initialization
     void Start () {
         model = GetComponent<PowerUpCoreModel>();
         rb = GetComponent<Rigidbody2D>();
-	}
+        if (transform.localPosition.y < 0)
+        {
+            //isClockWise = false;
+            transform.Rotate(0, 0, 0);
+        }
+        else
+        {
+            //isClockWise = true;
+            transform.Rotate(0, 0, 180);
+        }
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        Movement();
+        Movement2();
 	}
 
     void Movement()
@@ -29,19 +40,26 @@ public class PowerUpCoreMovement : MonoBehaviour {
 
         float value = Vector3.Cross(pointToTarget, transform.right).z;
 
-        rb.angularVelocity = 750 * value;
+        rb.angularVelocity = 700 * value;
 
-        rb.velocity = transform.right * 10;
+        rb.velocity = transform.right * 7;
     }
 
-    //void Movement2()
-    //{
-    //    Vector3 target = model.target.position;
-    //    Vector3 distance = target - transform.position;
+    void Movement2()
+    {
+        Vector3 target = model.target.position;
+        Vector3 distance = target - transform.position;
 
-    //    var angle = Mathf.Atan2(distance.y, distance.x) * Mathf.Rad2Deg - 90;
-    //    transform.rotation = Quaternion.Slerp(transform.rotation, (Quaternion.AngleAxis(angle, Vector3.forward)), turningSpeed * Time.deltaTime);
+        var angle = Mathf.Atan2(distance.y, distance.x) * Mathf.Rad2Deg - 90;
+        //if(isClockWise)
+        //{
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, (Quaternion.AngleAxis(angle, Vector3.back)), turningSpeed * Time.deltaTime);
+        //}
+        //else
+        //{
+        //}
+        transform.rotation = Quaternion.Slerp(transform.rotation, (Quaternion.AngleAxis(angle, Vector3.forward)), turningSpeed * Time.deltaTime);
 
-    //    transform.Translate(Vector3.up * speed * Time.deltaTime);
-    //}
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+    }
 }
